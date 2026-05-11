36a0e2631dcf53e1323731f8023241b3990145a7
cokkeijigen <itsukezigen@gmail.com>
2026-05-12 02:36:00 +0800

    ensure process exits if TVPProjectDir is not selected

--- base/win32/SysInitImpl.cpp
+++ base/win32/SysInitImpl.cpp
@@ -1161,7 +1161,7 @@ void TVPBeforeSystemInit()
 				}
 				TVPProjectDirSelected = true;
 			}
-			::CoTaskMemFree( pidlRetFolder );
+			::CoTaskMemFree( (LPVOID)pidlRetFolder );
 		}
 	}
 #endif
@@ -1170,6 +1170,10 @@ void TVPBeforeSystemInit()
 	if(TVPProjectDirSelected)
 	{
 		Application->SetShowMainForm( false );
+	}
+	else 
+	{
+		::ExitProcess(-1);
 	}
 
 	tjs_int buflen = (tjs_int)TJS_strlen(buf);
@@ -1182,10 +1186,7 @@ void TVPBeforeSystemInit()
 	TVPSetCurrentDirectory(TVPProjectDir);
 	TVPNativeProjectDir = buf;
 
-	if(TVPProjectDirSelected)
-	{
-		TVPAddImportantLog( TVPFormatMessage(TVPInfoSelectedProjectDirectory, TVPProjectDir) );
-	}
+	TVPAddImportantLog( TVPFormatMessage(TVPInfoSelectedProjectDirectory, TVPProjectDir) );
 }
 //---------------------------------------------------------------------------
 static void TVPDumpOptions();
