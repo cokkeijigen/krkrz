c4435a2eae24738761494ee6336c15bd3fd134cc
cokkeijigen <itsukezigen@gmail.com>
2026-05-12 02:12:41 +0800

    build: add CMake support for Clang and optimize build logic.

--- cmake/baseclasses.cmake
+++ cmake/baseclasses.cmake
@@ -1,5 +1,6 @@
 
-add_library(baseclasses STATIC 
+
+set(BASECLASSES_SOURCES
 	"${SOURCE_ROOT}/external/baseclasses/amextra.cpp"
 	"${SOURCE_ROOT}/external/baseclasses/amfilter.cpp"
 	"${SOURCE_ROOT}/external/baseclasses/amvideo.cpp"
@@ -28,10 +29,31 @@ add_library(baseclasses STATIC
 	"${SOURCE_ROOT}/external/baseclasses/vtrans.cpp"
 	"${SOURCE_ROOT}/external/baseclasses/winctrl.cpp"
 	"${SOURCE_ROOT}/external/baseclasses/winutil.cpp"
-	"${SOURCE_ROOT}/external/baseclasses/wxdebug.cpp"
 	"${SOURCE_ROOT}/external/baseclasses/wxlist.cpp"
 	"${SOURCE_ROOT}/external/baseclasses/wxutil.cpp"
 )
+
+set(ORIGINAL_WXDEBUG "${SOURCE_ROOT}/external/baseclasses/wxdebug.cpp")
+set(FIXED_WXDEBUG    "${CMAKE_BINARY_DIR}/baseclasses/wxdebug.cpp")
+
+if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+	list(APPEND BASECLASSES_SOURCES ${ORIGINAL_WXDEBUG})
+else()
+	if(EXISTS ${ORIGINAL_WXDEBUG})
+		file(READ ${ORIGINAL_WXDEBUG} FILE_CONTENTS)
+		string(
+			REPLACE "CDisp::CDisp(pp)" "(void)CDisp(pp)" 
+			MODIFIED_CONTENTS "${FILE_CONTENTS}"
+		)
+		file(WRITE ${FIXED_WXDEBUG} "${MODIFIED_CONTENTS}")
+	endif()
+
+	if(EXISTS ${FIXED_WXDEBUG})
+		list(APPEND BASECLASSES_SOURCES ${FIXED_WXDEBUG})
+	endif()
+endif()
+
+add_library(baseclasses STATIC ${BASECLASSES_SOURCES})
 
 target_include_directories(baseclasses PUBLIC
 	"${SOURCE_ROOT}/external/baseclasses/"
